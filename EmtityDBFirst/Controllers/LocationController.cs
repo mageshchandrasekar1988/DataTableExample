893874e786ff8c6867b33693a0a6b40f8f6cfb1d
Magesh CHANDRASEKAR <SSC.MACHANDRASEKAR@INCHESSC.COM>
2020-02-26 09:53:56 +0530

    Download Excal Implemented

--- EmtityDBFirst/Controllers/LocationController.cs
+++ EmtityDBFirst/Controllers/LocationController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using EmtityDBFirst.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using System.Linq.Dynamic;
 namespace EmtityDBFirst.Controllers
 {
     public class LocationController : Controller
@@ -21,10 +21,79 @@ namespace EmtityDBFirst.Controllers
             return View();
         }
         [HttpGet]
-        public JsonResult GetData() 
+        public JsonResult GetData1() 
         {
             List<Location> locations = context.Location.ToList();
             return Json(new { data = locations }, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+
+        [HttpGet]
+        public JsonResult GetData()
+        {
+            //Get parameters
+
+            //Get start (paging start index) and length (page size for paging)
+
+            int draw = Convert.ToInt32(Request.Query["draw"]);
+            int start = Convert.ToInt32(Request.Query["start"]); 
+            int length = Convert.ToInt32(Request.Query["length"]);
+
+            //Get sort columns value 
+            
+            int sortColumnIdx = Convert.ToInt32(Request.Query["order[0][column]"]);
+            string sortColumnName = Request.Query["columns[" + sortColumnIdx + "][name]"];
+            //    Request.Form["columns[" + order + "][name]"].FirstOrDefault();
+            var sortColumnDir = Request.Query["order[0][dir]"];
+
+            string searchValue = Request.Query["search[value]"].FirstOrDefault()?.Trim();
+            // Records Count matching search criteria
+            int recordFilteredCount = context.Location.Where(a => a.LocationName.Contains(searchValue)).Count();
+            // Total Records Count
+            int recordsTotalCount = context.Location.Count();
+            //Filter & sortred & paged data to  be sent to form server to view
+            List<Location> filteredData = null;
+            if(sortColumnDir == "asc")
+            {
+                filteredData = context.Location
+                    .Where(a => a.LocationName.Contains(searchValue))
+                    .OrderBy(x => x.GetType().GetProperty(sortColumnName).GetValue(x))
+                    .Skip(start)
+                    .Take(length)
+                    .ToList<Location>();
+            }
+            else
+            {
+                filteredData = context.Location.ToList();
+                /*filteredData = context.Location
+                   .Where(a => a.LocationName.Contains(searchValue))
+                   .OrderByDescending(x => x.GetType().GetProperty(sortColumnName).GetValue(x))
+                   .Skip(start)
+                   .Take(length)
+                   .ToList<Location>();*/
+            }
+
+            return Json(new
+            {
+                data = filteredData,
+                draw = Request.Query["draw"],
+                recordsFiltered = recordFilteredCount,
+                recordsTotal = recordsTotalCount
+            });
+            /*int pageSize = 0;//length != null ? length : 0;
+            int skip = 0;//start != null ? start : 0;
+            int totalRecord = 0;
+
+            var v = (from a in context.Location select a);
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+            {
+                v = v.OrderBy(sortColumn + " " + sortColumnDir);
+            }
+            totalRecord = v.Count();
+            var data = v.Skip(skip).Take(pageSize).ToList();
+            return Json(new { draw = draw, recordsFiltered = totalRecord, recordsTotal = totalRecord, data = data }, new Newtonsoft.Json.JsonSerializerSettings());
+            */
+            //List<Location> locations = context.Location.ToList();
+            // return Json(new { data = locations }, new Newtonsoft.Json.JsonSerializerSettings());
         }
         public IActionResult Datatable()
         {
--- EmtityDBFirst/Controllers/UserController.cs
+++ EmtityDBFirst/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using EmtityDBFirst.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace EmtityDBFirst.Controllers
 {
@@ -110,6 +113,70 @@ namespace EmtityDBFirst.Controllers
         {
             List<User> users = _context.User.ToList();
             return Json(new { data = users });
+        }
+
+        public IActionResult ExcelDownload()
+        {
+            //step1: create array to holder header labels
+            string[] col_name = new string[]
+            {
+                "First Name",
+                "Last Name"
+            };
+            //step2: create result byte array
+            byte[] result;
+            //step3: create a new package using memory safe structure
+            using (var package = new ExcelPackage())
+            {
+                //step4: create a new worksheet
+                var workSheet = package.Workbook.Worksheets.Add("Testing Sheet");
+                //step5: fill in header row
+                //worksheet.Cells[row,col].  {Style, Value}
+                for (int i=0; i<col_name.Length; i++)
+                {
+                    workSheet.Cells[1, i + 1].Style.Font.Size = 14;
+                    workSheet.Cells[1, i + 1].Value = col_name[i];
+                    workSheet.Cells[1, i + 1].Style.Font.Bold = true;
+                    //border the cell
+                    workSheet.Cells[1, i + 1].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                    //set background color for each sell
+                    workSheet.Cells[1, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    workSheet.Cells[1, i + 1].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(255, 243, 214));
+
+                }
+                int row = 2;
+                //step6: loop through query result and fill in cells
+                foreach (var item in _context.User.ToList())
+                {
+                    for (int col = 0; col <= 2; col++)
+                    {
+                        workSheet.Cells[row, 1].Style.Font.Size = 14;
+                        workSheet.Cells[row, 2].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                    }
+                    workSheet.Cells[row, 1].Value = item.FirstName;
+                    workSheet.Cells[row, 2].Value = item.LastName;
+
+                    //toggle background color
+                    //even row with ribbon style
+                    if (row % 2 == 0)
+                    {
+                        workSheet.Cells[row, 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        workSheet.Cells[row, 1].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(154, 211, 157));
+
+                        workSheet.Cells[row, 2].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        workSheet.Cells[row, 2].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(154, 211, 157));
+
+                    }
+                    row++;
+                }
+                //step7: auto fit columns
+                workSheet.Cells[workSheet.Dimension.Address].AutoFitColumns();
+                //step8: convert the package as byte array
+                result = package.GetAsByteArray();
+
+            }
+            //step9: return byte array as a file
+            return File(result, "application/vnd.ms-excel", "test.xls");
         }
     }
 }
